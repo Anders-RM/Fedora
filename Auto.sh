58fcbb9b7cb6e3b5ed9e9ed0808c9de1e402e098
anddersrm1808 <andersrm1808@gmail.com>
2023-01-17 16:46:53 +0100

    Test 17/01/2023

--- Auto.sh
+++ Auto.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 sudo dnf upgrade -y
-sudo dnf install powerline-fonts -y
+sudo dnf install zsh -y
 sudo dnf install git -y
+sudo dnf install powerline-fonts -y
 
-
+#hyper
+sudo bash -c 'rpm -i <(wget -O - https://releases.hyper.is/download/rpm)'
+#ohmyzsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+#powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#VSCode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 cat <<EOF | sudo tee /etc/yum.repos.d/vscode.repo
@@ -17,3 +23,4 @@ gpgkey=https://packages.microsoft.com/keys/microsoft.asc
 EOF
 dnf check-update
 sudo dnf install -y code
+
